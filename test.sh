042222b465a0446b2cba9dfd5dc94ebfd481926c
mrxbug <112347226+mrxbug@users.noreply.github.com>
2024-03-30 00:57:17 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Definindo cores
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # Sem cor
+
 # Banner
 banner(){
-cat<<"EOF" 
+cat << "EOF"
                ███╗   ███╗██████╗ ██╗  ██╗
-               ████╗ ████║██╔══██╗╚██╗██╔╝   
-               ██╔████╔██║██████╔╝ ╚███╔╝ 
-               ██║╚██╔╝██║██╔══██╗ ██╔██╗ 
+               ████╗ ████║██╔══██╗╚██╗██╔╝
+               ██╔████╔██║██████╔╝ ╚███╔╝
+               ██║╚██╔╝██║██╔══██╗ ██╔██╗
                ██║ ╚═╝ ██║██║  ██║██╔╝ ██╗
-               ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   
+               ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
       :::!~!!!!!:.
                   .xUHWH!! !!?M88WHX:.
                 .X*#M@$!!  !X!M$$$$$$WWx:.
@@ -20,7 +26,7 @@ cat<<"EOF"
                ~?WuxiW*`   `"#$$$$8!!!!??!!!
              :X- M$$$$       `"T#$T~!8$WUXU~
             :%`  ~#$$$m:        ~!~ ?$$$$$$
-          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"               
+          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
 .....   -~~:<` !    ~?T#$$@@W@*?$$      /`
 W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
 %"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
@@ -30,120 +36,119 @@ Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
 $R@i.~~ !     :   ~$$$$$B$$en:``
 ?MXT@Wx.~    :     ~"##*$$$$M~
 EOF
-echo -e "${BLUE}+ -- --=[Scanner by MRX"
-echo -e "${yellow}+ -- --= SEM LOGS SEM CRIME :)"
+echo -e "${BLUE}+ -- --=[Scanner by MRX${NC}"
+echo -e "${YELLOW}+ -- --= SEM LOGS SEM CRIME :)${NC}"
 }
+
+# Executando o banner
 banner
-echo -e "${RED} $1 Starting a vulnerability scan."
-sleep 4
 
-
-# Prompting user for domain input
+# Verificando a entrada do domínio
 read -p "Enter the domain: " domain
 
-# Checking if the domain is provided
+# Verificando se o domínio foi fornecido
 if [ -z "$domain" ]; then
-    echo "Error: Domain not provided."
+    echo -e "${RED}Error: Domain not provided.${NC}"
     exit 1
 fi
 
-# Create folder for the domain
+# Criando pasta para o domínio
 folder="$domain"
 mkdir -p "$folder"
 
-# Running subfinder to find subdomains
+# Executando subfinder para encontrar subdomínios
 echo "Running subfinder..."
 subfinder -d "$domain" -o "$folder/subdomains_tmp.txt"
 
-# Running amass to find subdomains
+# Executando amass para encontrar subdomínios
 echo "Running amass..."
 amass enum -passive -norecursive -d "$domain" -o "$folder/subdomains_tmp.txt"
 
-# Running Findomain
+# Executando Findomain
 echo "Running Findomain..."
 findomain -t $domain -q 2>/dev/null > "$folder/subdomains_tmp.txt"
 
-# Running Assetfinder
+# Executando Assetfinder
 echo "Running Assetfinder..."
 assetfinder --subs-only $domain > "$folder/subdomains_tmp.txt"
 
-# Running Sublist3r
+# Executando Sublist3r
 echo "Running Sublist3r..."
 sublist3r -d $domain -v -o "$folder/subdomains_tmp.txt"
 
-# Running jldc 
+# Executando jldc
 echo "Running jldc..."
- curl -s "https://jldc.me/anubis/subdomains/$domain" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | anew "$folder/subdomains_tmp.txt"
+curl -s "https://jldc.me/anubis/subdomains/$domain" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | anew "$folder/subdomains_tmp.txt"
 
-# Running wayback
-echo "Running wayback..." 
+# Executando wayback
+echo "Running wayback..."
 curl -sk "http://web.archive.org/cdx/search/cdx?url=*.$domain&output=txt&fl=original&collapse=urlkey&page=" | awk -F/ '{gsub(/:.*/, "", $3); print $3}' | sort -u | anew "$folder/subdomains_tmp.txt"
 
-# Running crt
+# Executando crt
 echo "Running crt..."
 curl -sk "https://crt.sh/?q=%.$domain&output=json" | tr ',' '\n' | awk -F'"' '/name_value/ {gsub(/\*\./, "", $4); gsub(/\\n/,"\n",$4);print $4}' | sort -u > "$folder/subdomains_tmp.txt"
 
-# Running abuseipdb
+# Executando abuseipdb
 echo "Running abuseipdb..."
 curl -s "https://www.abuseipdb.com/whois/$domain" -H "user-agent: firefox" -b "abuseipdb_session=" | grep -E '<li>\w.*</li>' | sed -E 's/<\/?li>//g' | sed -e "s/$/.$domain/" | sort -u > "$folder/subdomains_tmp.txt"
 
-# Running alienvault
+# Executando alienvault
 echo "Running alienvault..."
 curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq '.passive_dns[].hostname' 2>/dev/null | grep -o "\w.*$domain"|sort -u > "$folder/subdomains_tmp.txt"
 
-# Running urlscan.io
+# Executando urlscan.io
 echo "Running urlscan..."
 curl -s "https://urlscan.io/api/v1/search/?q=domain:$domain" | jq '.results[].page.domain' 2>/dev/null | grep -o "\w.*$domain"|sort -u > "$folder/subdomains_tmp.txt"
 
-# Running RapidDNS
+# Executando RapidDNS
 echo "Running RapidDNS..."
 curl -s "https://rapiddns.io/subdomain/$domain?full=1#result" | grep -v "RapidDNS" | grep -v "<td><a" | cut -d '>' -f 2 | cut -d '<' -f 1 | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | anew "$folder/subdomains_tmp.txt"
 
-# Running Naabu
+# Executando Naabu
 echo "Running naabu..."
 naabu -c 250 -l "$folder/subdomains_tmp.txt" -port "80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,3000,5000,8080,8000,8081,8888,8069,8009,8001,8070,8088,8002,8060,8091,8086,8010,8050,8085,8089,8040,8020,8051,8087,8071,8011,8030,8061,8072,8100,8083,8073,8099,8092,8074,8043,8035,8055,8021,8093,8022,8075,8044,8062,8023,8094,8012,8033,8063,8045,7000,9000,7070,9001,7001,10000,9002,7002,9003,7003,10001,80,443,4443" | anew "$folder/portscan.txt"
 echo "Running httprobe Live ports"
 cat "$folder/portscan.txt" | httprobe -c 25 > "$folder/liveports.txt"
 
-# Cleaning and sorting subdomains
+# Limpando e ordenando subdomínios
 echo "Cleaning and sorting subdomains..."
 sort -u "$folder/subdomains_tmp.txt" > "$folder/subdomains.txt"
 rm "$folder/subdomains_tmp.txt"
 
-# Running httpx to find live subdomains
+# Executando httprobe para encontrar subdomínios ativos
 echo "Finding live subdomains..."
 cat "$folder/subdomains.txt" | httprobe -c 25 > "$folder/live_subdomains.txt"
 
-# Running gau to find endpoints
+# Executando gau para encontrar endpoints
 echo "Finding endpoints with gau..."
-echo "$domain" | gau -t 5 >> "$folder/Endpoints.txt"
+echo "$domain" | gau --threads 5 >> "$folder/Endpoints.txt"
 
-# Running katana with -jc flag to find more endpoints
+# Executando katana com a flag -jc para encontrar mais endpoints
 echo "Finding endpoints with katana..."
 cat "$folder/live_subdomains.txt" | katana -d10 -jc >> "$folder/Endpoints.txt"
 
-# Removing duplicates using uro
+# Removendo duplicatas usando uro
 echo "Removing duplicates from Endpoints.txt..."
 uro -i "$folder/Endpoints.txt" -o "$folder/Endpoints.txt"
 
-# Using gf xss matcher to find XSS vulnerabilities
+# Usando gf xss matcher para encontrar vulnerabilidades XSS
 echo "Finding XSS vulnerabilities with gf..."
 cat "$folder/Endpoints.txt" | gf xss >> "$folder/xss.txt"
 
-# Using Gxss to send payloads to potential XSS endpoints
+# Usando Gxss para enviar payloads para endpoints XSS potenciais
 echo "Sending payloads with Gxss..."
 cat "$folder/xss.txt" | Gxss -p khXSS -o "$folder/XSS_Ref.txt"
 
-# Analyzing XSS references with dalfox
+# Analisando referências XSS com dalfox
 echo "Analyzing XSS references with dalfox..."
 dalfox file "$folder/XSS_Ref.txt" -o "$folder/Vulnerable_XSS.txt"
 
 echo "Vulnerable XSS endpoints saved in $folder/Vulnerable_XSS.txt"
 
-# nuclei Severity
-echo"Running Nuclei Severity..."
+# Nuclei Severity
+echo "Running Nuclei Severity..."
 cat "$folder/liveports.txt" | nuclei -severity info,low,medium,high,critical -o "$folder/severity.txt"
 
-# nuclei exposures,cves
-echo"Running Nuclei exposures,cves..."
+# Nuclei exposures,cves
+echo "Running Nuclei exposures,cves..."
 cat "$folder/live_subdomains.txt" | nuclei -t exposures/ -t cves/ -o "$folder/ExposuresCves.txt"
