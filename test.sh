d8cebc76886f65d56e2d9b7f4f4d56be04dcd1a3
mrxbug <112347226+mrxbug@users.noreply.github.com>
2024-03-31 18:29:49 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -58,7 +58,7 @@ mkdir -p "$folder"
 
 # Executando subfinder para encontrar subdomínios
 echo "Running subfinder..."
-subfinder -d "$domain" -o "$folder/subdomains_tmp.txt"
+subfinder -d "$domain" -all -o "$folder/subdomains_tmp.txt"
 
 # Executando amass para encontrar subdomínios
 echo "Running amass..."
@@ -66,11 +66,11 @@ amass enum -passive -norecursive -d "$domain" -o "$folder/subdomains_tmp.txt"
 
 # Executando Findomain
 echo "Running Findomain..."
-findomain -t $domain -q 2>/dev/null > "$folder/subdomains_tmp.txt"
+findomain -t $domain -q 2>/dev/null >> "$folder/subdomains_tmp.txt"
 
 # Executando Assetfinder
 echo "Running Assetfinder..."
-assetfinder --subs-only $domain > "$folder/subdomains_tmp.txt"
+assetfinder --subs-only $domain >> "$folder/subdomains_tmp.txt"
 
 # Executando Sublist3r
 echo "Running Sublist3r..."
@@ -78,31 +78,31 @@ sublist3r -d $domain -v -o "$folder/subdomains_tmp.txt"
 
 # Executando jldc
 echo "Running jldc..."
-curl -s "https://jldc.me/anubis/subdomains/$domain" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | anew "$folder/subdomains_tmp.txt"
+curl -s "https://jldc.me/anubis/subdomains/$domain" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | sort -u >> "$folder/subdomains_tmp.txt"
 
 # Executando wayback
 echo "Running wayback..."
-curl -sk "http://web.archive.org/cdx/search/cdx?url=*.$domain&output=txt&fl=original&collapse=urlkey&page=" | awk -F/ '{gsub(/:.*/, "", $3); print $3}' | sort -u | anew "$folder/subdomains_tmp.txt"
+curl -sk "http://web.archive.org/cdx/search/cdx?url=*.$domain&output=txt&fl=original&collapse=urlkey&page=" | awk -F/ '{gsub(/:.*/, "", $3); print $3}' | sort -u >> "$folder/subdomains_tmp.txt"
 
 # Executando crt
 echo "Running crt..."
-curl -sk "https://crt.sh/?q=%.$domain&output=json" | tr ',' '\n' | awk -F'"' '/name_value/ {gsub(/\*\./, "", $4); gsub(/\\n/,"\n",$4);print $4}' | sort -u > "$folder/subdomains_tmp.txt"
+curl -sk "https://crt.sh/?q=%.$domain&output=json" | tr ',' '\n' | awk -F'"' '/name_value/ {gsub(/\*\./, "", $4); gsub(/\\n/,"\n",$4);print $4}' | sort -u >> "$folder/subdomains_tmp.txt"
 
 # Executando abuseipdb
 echo "Running abuseipdb..."
-curl -s "https://www.abuseipdb.com/whois/$domain" -H "user-agent: firefox" -b "abuseipdb_session=" | grep -E '<li>\w.*</li>' | sed -E 's/<\/?li>//g' | sed -e "s/$/.$domain/" | sort -u > "$folder/subdomains_tmp.txt"
+curl -s "https://www.abuseipdb.com/whois/$domain" -H "user-agent: firefox" -b "abuseipdb_session=" | grep -E '<li>\w.*</li>' | sed -E 's/<\/?li>//g' | sed -e "s/$/.$domain/" | sort -u >> "$folder/subdomains_tmp.txt"
 
 # Executando alienvault
 echo "Running alienvault..."
-curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq '.passive_dns[].hostname' 2>/dev/null | grep -o "\w.*$domain"|sort -u > "$folder/subdomains_tmp.txt"
+curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq '.passive_dns[].hostname' 2>/dev/null | grep -o "\w.*$domain" | sort -u >> "$folder/subdomains_tmp.txt"
 
 # Executando urlscan.io
 echo "Running urlscan..."
-curl -s "https://urlscan.io/api/v1/search/?q=domain:$domain" | jq '.results[].page.domain' 2>/dev/null | grep -o "\w.*$domain"|sort -u > "$folder/subdomains_tmp.txt"
+curl -s "https://urlscan.io/api/v1/search/?q=domain:$domain" | jq '.results[].page.domain' 2>/dev/null | grep -o "\w.*$domain"| sort -u >> "$folder/subdomains_tmp.txt"
 
 # Executando RapidDNS
 echo "Running RapidDNS..."
-curl -s "https://rapiddns.io/subdomain/$domain?full=1#result" | grep -v "RapidDNS" | grep -v "<td><a" | cut -d '>' -f 2 | cut -d '<' -f 1 | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | anew "$folder/subdomains_tmp.txt"
+curl -s "https://rapiddns.io/subdomain/$domain?full=1#result" | grep -v "RapidDNS" | grep -v "<td><a" | cut -d '>' -f 2 | cut -d '<' -f 1 | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | sort -u >> "$folder/subdomains_tmp.txt"
 
 # Executando Naabu
 echo "Running naabu..."
@@ -125,7 +125,11 @@ cat "$folder/live_subdomains.txt" | egrep -i "internal|api|test|prod|private|sec
 
 # Executando gau para encontrar endpoints
 echo "Finding endpoints with gau..."
-echo "$domain" | gau --threads 5 >> "$folder/Endpoints.txt"
+cat "$folder/live_subdomains.txt" | gau --threads 5 >> "$folder/Endpoints.txt"
+
+# Executando waybackurls
+echo "Executando waybackurls..."
+cat "$folder/live_subdomains.txt" | waybackurls >> "$folder/Endpoints.txt"
 
 # Executando katana com a flag -jc para encontrar mais endpoints
 echo "Finding endpoints with katana..."
@@ -149,9 +153,12 @@ dalfox file "$folder/XSS_Ref.txt" -o "$folder/Vulnerable_XSS.txt"
 
 echo "Vulnerable XSS endpoints saved in $folder/Vulnerable_XSS.txt"
 
-# Nuclei Severity
+# Usando Running Nuclei Severity
 echo "Running Nuclei Severity..."
-cat "$folder/liveports.txt","$folder/live_subdomains.txt" | anew "$folder/nucleiDo.txt"
-cat "$folder/nucleiDo.txt" | nuclei -severity low,medium,high,critical -o "$folder/severity.txt"
+cat "$folder/live_subdomains.txt" | nuclei -severity low,medium,high,critical -o "$folder/severity.txt"
+
+# Usando Running Nuclei Custom Template
+echo "Running Nuclei Custom Templat..."
+cat "$folder/live_subdomains.txt" | nuclei -t ~/nuclei-templates/siteminder-dom-xss.yaml
 
 
