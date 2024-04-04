dd7000b43c217eb627f63a95ae400454a8657a87
mrxbug <112347226+mrxbug@users.noreply.github.com>
2024-04-04 00:20:10 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -58,62 +58,63 @@ mkdir -p "$folder"
 
 # Executando subfinder para encontrar subdomínios
 echo "Running subfinder..."
-subfinder -d "$domain" -all -o "$folder/subdomains_tmp.txt"
+subfinder -d "$domain" -all -o "$folder/subfinder_tmp.txt"
 
 # Executando amass para encontrar subdomínios
 echo "Running amass..."
-amass enum -passive -norecursive -d "$domain" -o "$folder/subdomains_tmp.txt"
+amass enum -passive -norecursive -d "$domain" -o "$folder/amass_tmp.txt"
 
 # Executando Findomain
 echo "Running Findomain..."
-findomain -t $domain -q 2>/dev/null > "$folder/subdomains_tmp.txt"
+findomain -t $domain -q 2>/dev/null > "$folder/Findomain_tmp.txt"
 
 # Executando Assetfinder
 echo "Running Assetfinder..."
-assetfinder --subs-only $domain > "$folder/subdomains_tmp.txt"
+assetfinder --subs-only $domain > "$folder/Assetfinder_tmp.txt"
 
 # Executando Sublist3r
 echo "Running Sublist3r..."
-sublist3r -d $domain -v -o "$folder/subdomains_tmp.txt"
+sublist3r -d $domain -v -o "$folder/Sublist3r_tmp.txt"
 
 # Executando jldc
 echo "Running jldc..."
-curl -s "https://jldc.me/anubis/subdomains/$domain" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | sort -u > "$folder/subdomains_tmp.txt"
+curl -s "https://jldc.me/anubis/subdomains/$domain" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | sort -u > "$folder/jldc_tmp.txt"
 
 # Executando wayback
 echo "Running wayback..."
-curl -sk "http://web.archive.org/cdx/search/cdx?url=*.$domain&output=txt&fl=original&collapse=urlkey&page=" | awk -F/ '{gsub(/:.*/, "", $3); print $3}' | sort -u > "$folder/subdomains_tmp.txt"
+curl -sk "http://web.archive.org/cdx/search/cdx?url=*.$domain&output=txt&fl=original&collapse=urlkey&page=" | awk -F/ '{gsub(/:.*/, "", $3); print $3}' | sort -u > "$folder/wayback_tmp.txt"
 
 # Executando crt
 echo "Running crt..."
-curl -sk "https://crt.sh/?q=%.$domain&output=json" | tr ',' '\n' | awk -F'"' '/name_value/ {gsub(/\*\./, "", $4); gsub(/\\n/,"\n",$4);print $4}' | sort -u > "$folder/subdomains_tmp.txt"
+curl -sk "https://crt.sh/?q=%.$domain&output=json" | tr ',' '\n' | awk -F'"' '/name_value/ {gsub(/\*\./, "", $4); gsub(/\\n/,"\n",$4);print $4}' | sort -u > "$folder/crt_tmp.txt"
 
 # Executando abuseipdb
 echo "Running abuseipdb..."
-curl -s "https://www.abuseipdb.com/whois/$domain" -H "user-agent: firefox" -b "abuseipdb_session=" | grep -E '<li>\w.*</li>' | sed -E 's/<\/?li>//g' | sed -e "s/$/.$domain/" | sort -u >> "$folder/subdomains_tmp.txt"
+curl -s "https://www.abuseipdb.com/whois/$domain" -H "user-agent: firefox" -b "abuseipdb_session=" | grep -E '<li>\w.*</li>' | sed -E 's/<\/?li>//g' | sed -e "s/$/.$domain/" | sort -u > "$folder/abuseipdb_tmp.txt"
 
 # Executando alienvault
 echo "Running alienvault..."
-curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq '.passive_dns[].hostname' 2>/dev/null | grep -o "\w.*$domain" | sort -u > "$folder/subdomains_tmp.txt"
+curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq '.passive_dns[].hostname' 2>/dev/null | grep -o "\w.*$domain" | sort -u > "$folder/alienvault_tmp.txt"
 
 # Executando urlscan.io
 echo "Running urlscan..."
-curl -s "https://urlscan.io/api/v1/search/?q=domain:$domain" | jq '.results[].page.domain' 2>/dev/null | grep -o "\w.*$domain"| sort -u > "$folder/subdomains_tmp.txt"
+curl -s "https://urlscan.io/api/v1/search/?q=domain:$domain" | jq '.results[].page.domain' 2>/dev/null | grep -o "\w.*$domain"| sort -u > "$folder/urlscan_tmp.txt"
 
 # Executando RapidDNS
 echo "Running RapidDNS..."
-curl -s "https://rapiddns.io/subdomain/$domain?full=1#result" | grep -v "RapidDNS" | grep -v "<td><a" | cut -d '>' -f 2 | cut -d '<' -f 1 | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | sort -u > "$folder/subdomains_tmp.txt"
-
-# Executando Naabu
-echo "Running naabu..."
-naabu -c 250 -l "$folder/subdomains_tmp.txt" -port "80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,3000,5000,8080,8000,8081,8888,8069,8009,8001,8070,8088,8002,8060,8091,8086,8010,8050,8085,8089,8040,8020,8051,8087,8071,8011,8030,8061,8072,8100,8083,8073,8099,8092,8074,8043,8035,8055,8021,8093,8022,8075,8044,8062,8023,8094,8012,8033,8063,8045,7000,9000,7070,9001,7001,10000,9002,7002,9003,7003,10001,80,443,4443" | anew "$folder/portscan.txt"
-echo "Running httpx Live ports"
-httpx -l "$folder/portscan.txt" -o "$folder/liveports.txt"
+curl -s "https://rapiddns.io/subdomain/$domain?full=1#result" | grep -v "RapidDNS" | grep -v "<td><a" | cut -d '>' -f 2 | cut -d '<' -f 1 | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | sort -u > "$folder/RapidDNS_tmp.txt"
 
 # Limpando e ordenando subdomínios
 echo "Cleaning and sorting subdomains..."
-sort -u "$folder/subdomains_tmp.txt" > "$folder/subdomains.txt"
-rm "$folder/subdomains_tmp.txt"
+cat "$folder/subfinder_tmp.txt" "$folder/amass_tmp.txt" "$folder/Findomain_tmp.txt" "$folder/Assetfinder_tmp.txt" "$folder/Sublist3r_tmp.txt" "$folder/jldc_tmp.txt" "$folder/wayback_tmp.txt" "$folder/crt_tmp.txt" "$folder/abuseipdb_tmp.txt" "$folder/alienvault_tmp.txt" "$folder/urlscan_tmp.txt" "$folder/RapidDNS_tmp.txt" > "$folder/subdomains_tmp1.txt" 
+sort -u "$folder/subdomains_tmp1.txt" > "$folder/subdomains.txt"
+rm "$folder/subfinder_tmp.txt" "$folder/amass_tmp.txt" "$folder/Findomain_tmp.txt" "$folder/Assetfinder_tmp.txt" "$folder/Sublist3r_tmp.txt" "$folder/jldc_tmp.txt" "$folder/wayback_tmp.txt" "$folder/crt_tmp.txt" "$folder/abuseipdb_tmp.txt" "$folder/alienvault_tmp.txt" "$folder/urlscan_tmp.txt" "$folder/RapidDNS_tmp.txt"
+
+# Executando Naabu
+echo "Running naabu..."
+naabu -c 250 -l "$folder/subdomains.txt" -port "80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,3000,5000,8080,8000,8081,8888,8069,8009,8001,8070,8088,8002,8060,8091,8086,8010,8050,8085,8089,8040,8020,8051,8087,8071,8011,8030,8061,8072,8100,8083,8073,8099,8092,8074,8043,8035,8055,8021,8093,8022,8075,8044,8062,8023,8094,8012,8033,8063,8045,7000,9000,7070,9001,7001,10000,9002,7002,9003,7003,10001,80,443,4443" | anew "$folder/portscan.txt"
+echo "Running httpx Live ports"
+httpx -l "$folder/portscan.txt" -o "$folder/liveports.txt"
 
 # Executando httpx para encontrar subdomínios ativos
 echo "Finding live subdomains..."
@@ -125,7 +126,7 @@ cat "$folder/live_subdomains.txt" | egrep -i "internal|api|test|prod|private|sec
 
 # Arquivos rápidos e suculentos com lista de palavras tomnomnom e ffuf
 echo "ffuf Arquivos suculentos..."
-ffuf -w ~/wordlists/common-paths-tom.txt -u "$domain/FUZZ" -o "$folder/ffuf.txt"
+ffuf -w ~/wordlists/common-paths-tom.txt -u "https://$domain/FUZZ" -o "$folder/ffuf.txt"
 
 # Extract .js Subdomains
 echo "Extract .js Subdomains..."
